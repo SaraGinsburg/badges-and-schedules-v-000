09b8297252a560251505e47d706abd968f956c5f
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-07 05:19:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+speakers = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+badges   =  []
+
+def badge_maker(name)
+  "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(speakers)
+  speakers.each do |person|
+    badges << badge_maker(person)
+  end
+  badges
+end
