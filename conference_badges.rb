43024e0f3b75f45493e84b676c64bff985477185
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-07 05:35:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,13 @@
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+# speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+speakers_arr = []
 badges   =  []
 
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(arr)
-  arr.each do |person|
+def batch_badge_creator(speakers_arr)
+  speakers_arr.each do |person|
     badges << badge_maker(person)
   end
   badges
