dc28fadca4fb868b9f0d33806d560113049df8b6
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-07 05:59:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 # speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-speakers_arr = []
-badges   =  []
+# speakers_arr = []
+# badges   =  []
 
 def badge_maker(name)
   "Hello, my name is #{name}."
@@ -8,8 +8,8 @@ end
 
 def batch_badge_creator(speakers_arr)
   speakers_arr.map {|speaker| badge_maker(speaker)}
-
 end
 
 def assign_rooms(attendees)
+  
 end
