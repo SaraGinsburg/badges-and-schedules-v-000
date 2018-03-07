53ce8128d4469642abce85951525564793b6f14e
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-03-07 05:30:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 badges   =  []
 
 def badge_maker(name)
-  "Hello, my name is #{name}"
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(speakers)
